d0851c384455771d6e898ee4705cb2dc9c327f79
Mark Hall <mark.hall@work.room3b.eu>
2024-09-27 15:07:47 +0100

    Reworked the books processing

--- llm_complex_leisure_search/books/data.py
+++ llm_complex_leisure_search/books/data.py
@@ -12,56 +12,34 @@ Request: "{request}"
 Please provide a ranked list of your 20 best guesses for the correct answer. Please answer in a JSON object that contains a ranked list of suggestions. Each suggestion should contain a field called 'answer' containing the suggestion (title and author), a field 'explanation' containing an explanation of why these books could be the correct answer, and a 'confidence' score that represents how confident you are of your suggestion."""  # noqa: E501
 
 
-def extract_solved_threads(posts: list[dict]) -> list[dict]:
+def extract_solved_threads(first_posts: list[dict], posts: list[dict], ignored_ids: list[str]) -> list[dict]:
     """Extract all solved threads from a list of posts."""
     solved = []
-    thread_id = None
-    solution = {
-        "confirmed": False,
-        "prompt": None,
-        "request": None,
-        "solved": False,
-        "thread_id": None,
-        "title": None,
-        "author": None,
-    }
-    for post in track(posts, description="Extracting solved threads"):
-        if thread_id != post["thread_id"]:
-            if solution["solved"]:
-                solved.append(solution)
-            thread_id = post["thread_id"]
-            solution = {
-                "confirmed": False,
-                "prompt": None,
-                "request": None,
-                "solved": False,
-                "thread_id": None,
-                "title": None,
-                "author": None,
-            }
-        if solution["thread_id"] is None:
-            solution["thread_id"] = post["thread_id"]
-            solution["request"] = post["comment_text"]
-            solution["prompt"] = PROMPT_TEMPLATE.format(request=solution["request"])
-        if (
-            post["solved"] == "solved"  # If the post is solved
-            and ":" in post["answer"]  # and there is a semi-colon (author - title)
-            and not solution["solved"]  # and it has not already been marked as solved
-        ):
-            solution["solved"] = True
-            solution["author"] = post["answer"][: post["answer"].find(":")].strip()
-            solution["title"] = post["answer"][post["answer"].find(":") + 1 :].strip()
-        elif (
-            post["solved"] == "solved and confirmed"  # If the post is solved
-            and ":" in post["answer"]  # and there is a semi-colon (author - title)
-            and not solution["solved"]  # and it has not already been marked as solved
-        ):
-            solution["solved"] = True
-            solution["author"] = post["answer"][: post["answer"].find(":")].strip()
-            solution["title"] = post["answer"][post["answer"].find(":") + 1 :].strip()
-            solution["confirmed"] = True
-        elif post["solved"] == "confirmed":
-            solution["confirmed"] = True
-    if solution["solved"]:
+    for first_post in track(first_posts, description="Finding solutions"):
+        if first_post["thread_id"] in ignored_ids:
+            continue
+        solution = {
+            "thread_id": first_post["thread_id"],
+            "request": first_post["request"],
+            "prompt": PROMPT_TEMPLATE.format(request=first_post["request"]),
+            "title": None,
+            "author": None,
+        }
+        for post in posts:
+            if post["thread_id"] != first_post["thread_id"]:
+                continue
+            if (
+                post["solved"] == "solved"  # If the post is solved
+                and ":" in post["answer"]  # and there is a semi-colon (author - title)
+            ):
+                solution["author"] = post["answer"][: post["answer"].find(":")].strip()
+                solution["title"] = post["answer"][post["answer"].find(":") + 1 :].strip()
+            elif (
+                post["solved"] == "solved / confirmed"  # If the post is solved
+                and ":" in post["answer"]  # and there is a semi-colon (author - title)
+            ):
+                solution["author"] = post["answer"][: post["answer"].find(":")].strip()
+                solution["title"] = post["answer"][post["answer"].find(":") + 1 :].strip()
+
         solved.append(solution)
     return solved
--- llm_complex_leisure_search/cli/books.py
+++ llm_complex_leisure_search/cli/books.py
@@ -18,102 +18,103 @@ from llm_complex_leisure_search.gemini import generate_multiple_responses
 from llm_complex_leisure_search.util import split_book_title_by_author
 
 group = Typer(name="books", help="Commands for book-related processing")
-ANNOTATION_SOURCE_FILES = ["posts_jdoc.csv", "posts_extra.csv"]
+ANNOTATION_SOURCE_FILES = ["jdoc", "extra"]
 
 
 @group.command()
 def extract() -> None:
     """Extract all solved book threads."""
-    records = []
-    for filename in ANNOTATION_SOURCE_FILES:
-        with open(os.path.join("data", "books", filename)) as in_f:
+    for suffix in ANNOTATION_SOURCE_FILES:
+        first_posts = []
+        with open(os.path.join("data", "books", f"first-posts_{suffix}.tsv")) as in_f:
+            reader = DictReader(in_f, delimiter="\t")
+            for record in reader:
+                first_posts.append(record)
+        records = []
+        with open(os.path.join("data", "books", f"posts_{suffix}.csv")) as in_f:
             reader = DictReader(in_f)
             for record in reader:
                 records.append(record)
-    solved = extract_solved_threads(records)
-    with open(os.path.join("data", "books", "solved.json"), "w") as out_f:
-        json.dump(solved, out_f)
+        with open(os.path.join("data", "books", f"ignored_{suffix}.txt")) as in_f:
+            ignored = [thread_id.strip() for thread_id in in_f.readlines() if thread_id.strip()]
+        solved = extract_solved_threads(first_posts, records, ignored)
+        with open(os.path.join("data", "books", f"solved_{suffix}.json"), "w") as out_f:
+            json.dump(solved, out_f)
 
 
 @group.command()
 def query_gemini() -> None:
     """Process the books with Gemini."""
-    with open(os.path.join("data", "books", "solved.json")) as in_f:
-        tasks = json.load(in_f)
-    results = []
-    for task in track(tasks, description="Querying Gemini"):
-        result = {
-            "thread_id": task["thread_id"],
-            "results": generate_multiple_responses(task["prompt"]),
-        }
-        for attempt in result["results"]:
-            if attempt is not None:
-                for entry in attempt:
-                    title, author = split_book_title_by_author(entry["answer"])
-                    entry["title"] = title
-                    entry["author"] = author
-        results.append(result)
-    with open(os.path.join("data", "books", "gemini.json"), "w") as out_f:
-        json.dump(results, out_f)
+    for suffix in ANNOTATION_SOURCE_FILES:
+        with open(os.path.join("data", "books", f"solved_{suffix}.json")) as in_f:
+            tasks = json.load(in_f)
+        results = []
+        for task in track(tasks, description="Querying Gemini"):
+            result = {
+                "thread_id": task["thread_id"],
+                "results": generate_multiple_responses(task["prompt"]),
+            }
+            for attempt in result["results"]:
+                if attempt is not None:
+                    for entry in attempt:
+                        title, author = split_book_title_by_author(entry["answer"])
+                        entry["title"] = title
+                        entry["author"] = author
+            results.append(result)
+        with open(os.path.join("data", "books", f"gemini_{suffix}.json"), "w") as out_f:
+            json.dump(results, out_f)
 
 
 @group.command()
 def stats() -> None:
     """Summary statistics for the books data-set."""
-    thread_ids = set()
-    for filename in ANNOTATION_SOURCE_FILES:
-        with open(os.path.join("data", "books", filename)) as in_f:
+    console("Book stats\n##########")
+    for suffix in ANNOTATION_SOURCE_FILES:
+        thread_ids = set()
+        with open(os.path.join("data", "books", f"posts_{suffix}.csv")) as in_f:
             reader = DictReader(in_f)
             for record in reader:
                 thread_ids.add(record["thread_id"])
-    total = len(thread_ids)
-    solved = 0
-    confirmed = 0
-    with open(os.path.join("data", "books", "solved.json")) as in_f:
-        tasks = json.load(in_f)
-        for task in tasks:
-            if task["solved"]:
-                solved = solved + 1
-            if task["confirmed"]:
-                confirmed = confirmed + 1
-    with open(os.path.join("data", "books", "gemini.json")) as in_f:
-        gemini_tasks = json.load(in_f)
-        gemini_solution = 0
-        gemini_solved = 0
-        gemini_solution_ranks = []
-        for gemini_task in gemini_tasks:
-            found = False
-            for task in tasks:
-                if task["thread_id"] == gemini_task["thread_id"]:
-                    found = True
-                    break
-            if found:
-                if len(gemini_task["results"]) > 0:
-                    gemini_solution = gemini_solution + 1
-                for result_list in gemini_task["results"]:
-                    if result_list[0]["title"] == task["title"]:
-                        gemini_solved = gemini_solved + 1
+        console(f"Total threads: {suffix}: {len(thread_ids)}")
+    for suffix in ANNOTATION_SOURCE_FILES:
+        with open(os.path.join("data", "books", f"solved_{suffix}.json")) as in_f:
+            tasks = json.load(in_f)
+        console(f"Solved: {suffix}: {len(tasks)}")
+    for suffix in ANNOTATION_SOURCE_FILES:
+        with open(os.path.join("data", "books", f"solved_{suffix}.json")) as in_f:
+            tasks = json.load(in_f)
+        with open(os.path.join("data", "books", f"gemini_{suffix}.json")) as in_f:
+            gemini_tasks = json.load(in_f)
+            gemini_solution = 0
+            gemini_solved = 0
+            gemini_solution_ranks = []
+            for gemini_task in gemini_tasks:
+                found = False
+                for task in tasks:
+                    if task["thread_id"] == gemini_task["thread_id"]:
+                        found = True
                         break
-                gemini_solution_rank = None
-                for result_list in gemini_task["results"]:
-                    for idx, result in enumerate(result_list):
-                        if result["title"] == task["title"]:
-                            if gemini_solution_rank is None:
-                                gemini_solution_rank = idx
-                            else:
-                                gemini_solution_rank = min(gemini_solution_rank, idx)
-                if gemini_solution_rank is not None:
-                    gemini_solution_ranks.append(gemini_solution_rank)
-
-    console(
-        f"""Book Stats
-==========
-Total threads: {total}
-Solved: {solved} ({solved/total:.2f})
-Confirmed: {confirmed} ({confirmed/total:.2f})
-Gemini Solution: {gemini_solution} ({gemini_solution/total:.2f})
-Gemini Solved: {gemini_solved} ({gemini_solved/total:.2f})
-Gemini Found: {len(gemini_solution_ranks)} ({len(gemini_solution_ranks)/total:.2f})
-Gemini Avg Rank: {sum(gemini_solution_ranks) / len(gemini_solution_ranks)}
-"""
-    )
+                if found:
+                    if len(gemini_task["results"]) > 0:
+                        gemini_solution = gemini_solution + 1
+                    for result_list in gemini_task["results"]:
+                        if result_list[0]["title"] == task["title"]:
+                            gemini_solved = gemini_solved + 1
+                            break
+                    gemini_solution_rank = None
+                    for result_list in gemini_task["results"]:
+                        for idx, result in enumerate(result_list):
+                            if result["title"] == task["title"]:
+                                if gemini_solution_rank is None:
+                                    gemini_solution_rank = idx
+                                else:
+                                    gemini_solution_rank = min(gemini_solution_rank, idx)
+                    if gemini_solution_rank is not None:
+                        gemini_solution_ranks.append(gemini_solution_rank)
+            console(f"Gemini Solution: {suffix}: {gemini_solution} ({gemini_solution / len(tasks):.2f})")
+            console(f"Gemini Solved: {suffix}: {gemini_solved} ({gemini_solved / len(tasks):.2f})")
+            console(
+                f"Gemini Found: {suffix}: {len(gemini_solution_ranks)} ({len(gemini_solution_ranks) / len(tasks):.2f})"
+            )
+            if (len(gemini_solution_ranks)) > 0:
+                console(f"Gemini Average Rank: {suffix}: {sum(gemini_solution_ranks) / len(gemini_solution_ranks)}")
