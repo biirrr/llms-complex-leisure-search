db90e2206c04f19302c5433dadb20693ab26e838
Mark Hall <mark.hall@work.room3b.eu>
2024-10-11 16:30:01 +0100

    Docs fix

--- llm_complex_leisure_search/cli/fix.py
+++ llm_complex_leisure_search/cli/fix.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2024-present Mark Hall <mark.hall@work.room3b.eu>
 #
 # SPDX-License-Identifier: MIT
-"""Books-related CLI commands."""
+"""Data fix-related CLI commands."""
 
 import json
 import os
