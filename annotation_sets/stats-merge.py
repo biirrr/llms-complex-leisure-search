42c8da7b7fa949426a9de9b0b2af762ca82b4053
Mark Hall <mark.hall@work.room3b.eu>
2025-09-26 10:54:17 +0100

    Stats merge scripts

---
+++ annotation_sets/stats-merge.py
@@ -0,0 +1,39 @@
+"""Merge stats from Toine's data."""
+
+import json
+from csv import DictReader
+
+ANNOTATED_FILE = "../../why-ki-requests-are-hard/data/annotated/games.json"
+STATS_FILE = "/home/mhall/Downloads/statistics.games.reddit-spring2025.tsv"
+
+with open(ANNOTATED_FILE) as in_f:
+    annotated = json.load(in_f)
+
+with open(STATS_FILE) as in_f:
+    reader = DictReader(in_f, delimiter="\t")
+    for line in reader:
+        for entry in annotated:
+            if entry["data"]["thread_id"] == line["thread_id"]:
+                entry["stats"] = {}
+                for key in [
+                    "title_length_chars",
+                    "text_length_chars",
+                    "full_post_length_chars",
+                    "title_length_words",
+                    "text_length_words",
+                    "full_post_length_words",
+                    "title_readability",
+                    "text_readability",
+                    "full_post_readability",
+                    "reply_counter",
+                    "replies_until_solved",
+                    "replies_until_confirmed",
+                    "OP_reply_count",
+                    "OP_reply_count_before_confirmed",
+                    "solved_by_OP",
+                    "unique_user_replies",
+                ]:
+                    entry["stats"][key] = line[key]
+
+with open(ANNOTATED_FILE, "w") as out_f:
+    json.dump(annotated, out_f)
