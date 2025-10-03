50114e0b12ffe3984e9c5403168a7076b1c5f05b
Mark Hall <mark.hall@work.room3b.eu>
2025-10-03 17:15:58 +0100

    Small fix

--- annotation_sets/stats-merge.py
+++ annotation_sets/stats-merge.py
@@ -3,42 +3,54 @@
 import json
 from csv import DictReader
 
-ANNOTATED_FILE = "../../why-ki-requests-are-hard/data/annotated/games.json"
-STATS_FILE = "/home/mhall/Downloads/statistics.games.reddit-spring2025.tsv"
+for annotated_file, stats_file in [
+    (
+        "../../why-ki-requests-are-hard/data/annotated/books.json",
+        "/home/mhall/Downloads/statistics/statistics.books.reddit-spring+summer2025.tsv",
+    ),
+    (
+        "../../why-ki-requests-are-hard/data/annotated/games.json",
+        "/home/mhall/Downloads/statistics/statistics.games.reddit-spring2025.tsv",
+    ),
+    (
+        "../../why-ki-requests-are-hard/data/annotated/movies.json",
+        "/home/mhall/Downloads/statistics/statistics.movies.reddit-spring2025.tsv",
+    ),
+]:
+    with open(annotated_file) as in_f:
+        annotated = json.load(in_f)
 
-with open(ANNOTATED_FILE) as in_f:
-    annotated = json.load(in_f)
+    with open(stats_file) as in_f:
+        reader = DictReader(in_f, delimiter="\t")
+        for line in reader:
+            for entry in annotated:
+                if str(entry["data"]["thread_id"]) == str(line["thread_id"]):
+                    if "stats" not in entry:
+                        entry["stats"] = {}
+                    for key in [
+                        "title_length_chars",
+                        "text_length_chars",
+                        "full_post_length_chars",
+                        "title_length_words",
+                        "text_length_words",
+                        "full_post_length_words",
+                        "title_readability",
+                        "text_readability",
+                        "full_post_readability",
+                        "reply_counter",
+                        "replies_until_solved",
+                        "replies_until_confirmed",
+                        "OP_reply_count",
+                        "OP_reply_count_before_confirmed",
+                        "solved_by_OP",
+                        "unique_user_replies",
+                    ]:
+                        try:
+                            entry["stats"][key] = float(line[key])
+                        except ValueError:
+                            if key in entry["stats"]:
+                                del entry["stats"][key]
+                            pass
 
-with open(STATS_FILE) as in_f:
-    reader = DictReader(in_f, delimiter="\t")
-    for line in reader:
-        for entry in annotated:
-            if entry["data"]["thread_id"] == line["thread_id"]:
-                entry["stats"] = {}
-                for key in [
-                    "title_length_chars",
-                    "text_length_chars",
-                    "full_post_length_chars",
-                    "title_length_words",
-                    "text_length_words",
-                    "full_post_length_words",
-                    "title_readability",
-                    "text_readability",
-                    "full_post_readability",
-                    "reply_counter",
-                    "replies_until_solved",
-                    "replies_until_confirmed",
-                    "OP_reply_count",
-                    "OP_reply_count_before_confirmed",
-                    "solved_by_OP",
-                    "unique_user_replies",
-                ]:
-                    try:
-                        entry["stats"][key] = float(line[key])
-                    except ValueError:
-                        if key in entry["stats"]:
-                            del entry["stats"][key]
-                        pass
-
-with open(ANNOTATED_FILE, "w") as out_f:
-    json.dump(annotated, out_f)
+    with open(annotated_file, "w") as out_f:
+        json.dump(annotated, out_f)
